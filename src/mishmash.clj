8f89c98ad8b0c6160ee2c51836fd74d5e8cebdd9
jake612 <jake612@live.unc.edu>
2020-01-28 15:37:03 -0500

    Initial Commit

---
+++ src/mishmash.clj
@@ -0,0 +1,26 @@
+(ns mishmash)
+
+(require '[clojure.string :as str])
+
+(defn ! [x] (reduce * 1 (range 1 (inc x))))
+
+(defn pascal [level]
+  (let [level (Integer/parseInt level)]
+    (if (> level -1)
+      (do
+        (print (str/join " " (map #(/ (! level) (* (! %) (! (- level %)))) (range (+ level 1))))))
+      (println "invalid input"))
+    )
+  )
+
+
+(defn write-roman [num])
+
+(defn read-roman [numerals])
+
+(defn -main [& args]
+  (cond
+    (= (first args) "pascal") (pascal (nth args 1))
+    (= (first args) "write-roman") (write-roman (rest args))
+    (= (first args) "read-roman") (read-roman (rest args))
+    ))
