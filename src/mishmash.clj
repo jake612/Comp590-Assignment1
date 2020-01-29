7f03c26bf5adbb393019b81e81e2606451d19c21
jake612 <jake612@live.unc.edu>
2020-01-29 10:50:31 -0500

    first attermpt at read-roman

--- src/mishmash.clj
+++ src/mishmash.clj
@@ -2,12 +2,26 @@
 
 (require '[clojure.string :as str])
 
-(defn ! [x] (reduce * 1 (range 1 (inc x))))
+(def numeral-chart {:I 1
+                    :V 5
+                    :X 10
+                    :L 50
+                    :C 100
+                    :D 500
+                    :M 1000
+                    :CM 900
+                    :XC 90
+                    :IX 9
+                    :IV 4
+                    :XL 40
+                    :CD 400
+                    :O 0})
 
 (defn pascal [input]
   (try
     (let [
           level (Integer/parseInt input)
+          ! (fn [x] (reduce * 1 (range 1 (inc x))))
           ]
       (if (> level -1)
         (do
@@ -21,7 +35,31 @@
 
 (defn write-roman [num])
 
-(defn read-roman [numerals])
+(defn numeral-reader [[total past-char] char]
+  (try
+    (let [
+          past-val (get numeral-chart (keyword past-char))
+          char-val (get numeral-chart (keyword char))
+          ]
+      (if (< past-val char-val)
+        [(+ total (* -2 past-val) char-val) char]
+        [(+ total char-val) char])
+      )
+    (catch Exception e
+      (println "invalid input")))
+  )
+
+(defn read-roman [numerals]
+  (try
+    (let [
+          result (first (reduce numeral-reader [0, "O"] (str/split  (first numerals) #"")))
+          ]
+      (if (some? result)
+          (println result)))
+    (catch Exception e (println "invalid input")))
+  )
+
+
 
 (defn -main [& args]
   (cond
